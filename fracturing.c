30d35afd37a89d56a3c693aea60c953d1ab2194c
ShreyanshMon <shreyansh.mdl@gmail.com>
2024-09-07 19:43:13 +0000

    fracturing

---
+++ fracturing.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#define PI = 3.14159;
+
+int main(int argc, char **argv)
+{
+    calculateDistance () ;
+    calculatePerimeter();
+    calculateArea () ;
+    calculateWidth () ;
+    calculateHeight () ;
+
+    return 0;
+}
+
+double calculateDistance()
+{
+
+}
+
+double calculatePerimeter()
+{
+
+}
+
+double calculateArea()
+{
+
+}
+
+double calculateWidth()
+{
+
+}
+
+double calculateHeight()
+{
+    
+}
+
+double askForUserInput()
+{
+    double num;
+    scanf(%lf, &num);
+    return num;
+}
